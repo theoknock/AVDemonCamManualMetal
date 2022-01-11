7fb48841b76041f6c90bbf8dc3c3b1c8417c4632
James Alan Bush <jamesalanbush1972@gmail.com>
2022-01-11 18:09:26 -0500

    Initial Commit

---
+++ AVDemonCamManualMetal/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  AVDemonCamManualMetal
+//
+//  Created by Xcode Developer on 1/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ AVDemonCamManualMetal/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AVDemonCamManualMetal
+//
+//  Created by Xcode Developer on 1/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
